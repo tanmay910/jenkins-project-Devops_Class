c173f1d18a018068f6beb3a8e7194f71fd57757e
tanmay910 <mahajantanmay910@gmail.com>
2024-10-24 16:01:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express'); // Fix mismatched quotes
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {  // Fix 'reg' to 'req' and mismatched quotes
+  res.send('Welcome to My Node.js App!'); // Fix mismatched quotes
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`); // Fix $ (PORT to ${PORT}
+});
